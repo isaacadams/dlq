20e5cbbc930d8cbb9bdaa76cf5c3ea69d0ef573e
Isaac Adams <26559009+isaacadams@users.noreply.github.com>
2024-05-05 00:03:09 -0400

    init

---
+++ crates/dlq-core/src/sqs.rs
@@ -0,0 +1,124 @@
+use anyhow::Context;
+use aws_config::SdkConfig;
+use aws_sdk_sqs as sqs;
+
+pub async fn list() {
+    let dlq = DeadLetterQueue::new().await;
+    let queues = dlq.list().await;
+    dbg!(queues);
+}
+
+pub async fn poll() {
+    let dlq = DeadLetterQueue::new().await;
+    let queue_url = env!("DLQ_URL");
+    dlq.poll(queue_url).await;
+}
+
+pub async fn receive(
+    client: &aws_sdk_sqs::Client,
+    queue_url: &str,
+) -> anyhow::Result<aws_sdk_sqs::operation::receive_message::ReceiveMessageOutput> {
+    let result = client
+        .receive_message()
+        .set_queue_url(Some(queue_url.to_string()))
+        .set_max_number_of_messages(Some(10))
+        .set_visibility_timeout(Some(15))
+        .set_attribute_names(Some(vec!["All".into()]))
+        //.set_wait_time_seconds(Some(3))
+        .send()
+        .await;
+
+    result.context("failed to receive messages")
+}
+
+#[derive(Clone)]
+struct DeadLetterQueue {
+    pub config: SdkConfig,
+    pub client: sqs::Client,
+}
+
+impl DeadLetterQueue {
+    pub async fn new() -> Self {
+        let config = aws_config::load_from_env().await;
+        let client = aws_sdk_sqs::Client::new(&config);
+        Self { config, client }
+    }
+
+    pub async fn list(&self) -> Vec<String> {
+        let mut queues = Vec::new();
+
+        let mut output = self.client.list_queues().send().await.unwrap();
+        loop {
+            if let Some(mut list) = output.queue_urls {
+                queues.append(&mut list);
+            }
+
+            let Some(token) = output.next_token else {
+                break;
+            };
+
+            output = self
+                .client
+                .list_queues()
+                .set_next_token(Some(token))
+                .send()
+                .await
+                .unwrap();
+        }
+
+        queues
+    }
+
+    pub async fn poll(&self, queue_url: &str) {
+        loop {
+            let output = receive(&self.client, queue_url).await.unwrap();
+
+            // if none, that suggests the whole queue has been received recently
+            let Some(messages) = output.messages else {
+                return;
+            };
+
+            for m in messages {
+                println!(
+                    "{}",
+                    serde_json::to_string(&MessageModel::from_aws_message(m)).unwrap()
+                );
+            }
+        }
+    }
+}
+
+#[derive(Clone, Debug, serde::Serialize)]
+pub struct MessageModel {
+    pub message_id: String,
+    receipt_handle: String,
+    md5_of_body: String,
+    pub body: String,
+    md5_of_message_attributes: Option<String>,
+    attributes: Option<String>,
+    message_attributes: Option<String>,
+}
+
+impl MessageModel {
+    /// https://docs.aws.amazon.com/AWSSimpleQueueService/latest/APIReference/API_Message.html
+    pub fn from_aws_message(message: aws_sdk_sqs::types::Message) -> Self {
+        Self {
+            message_id: message.message_id.expect("missing message_id"),
+            receipt_handle: message.receipt_handle.expect("missing receipt_handle"),
+            md5_of_body: message.md5_of_body.expect("missing md5_of_body"),
+            body: message.body.expect("missing body"),
+            md5_of_message_attributes: message.md5_of_message_attributes,
+            attributes: None,         //value.attributes,
+            message_attributes: None, //value.message_attributes,
+        }
+    }
+}
+
+mod test {
+    use super::*;
+
+    #[tokio::test]
+    async fn test_list() {
+        list().await;
+    }
+}
