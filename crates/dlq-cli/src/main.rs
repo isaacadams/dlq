8f34caf88f46210c6036af4c21ed6708dd382585
Isaac Adams <26559009+isaacadams@users.noreply.github.com>
2024-05-05 00:13:05 -0400

    creating dlq cli

--- crates/dlq-cli/src/main.rs
+++ crates/dlq-cli/src/main.rs
@@ -1,5 +1,33 @@
+use clap::{Parser, Subcommand};
+
 #[tokio::main]
-async fn main() {
-    dlq::list().await;
-    ()
+pub async fn main() {
+    if let Err(e) = Cli::parse().run().await {
+        eprintln!("{}", e);
+    }
+}
+
+#[derive(Debug, Parser)]
+#[command(name = "dlq")]
+#[command(about = "aws dead letter queue CLI client written in rust", long_about = None)]
+pub struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Debug, Subcommand)]
+enum Commands {
+    List,
+    Poll { url: Option<String> },
+}
+
+impl Cli {
+    pub async fn run(self) -> anyhow::Result<()> {
+        match self.command {
+            Commands::List => dlq::list().await,
+            Commands::Poll { url: _ } => dlq::poll().await,
+        };
+
+        Ok(())
+    }
 }
